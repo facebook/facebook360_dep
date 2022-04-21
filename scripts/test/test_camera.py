54c5b8261c30514782b0192c0bf5323b57073c67
Kosie van der Merwe <kosievdmerwe@fb.com>
2022-04-21 13:36:01 -0700

    Meta license header migration
    Summary:
    # Overview
    As we renamed our company we need to update our license headers to Meta.
    
    Differential Revision: D35818342
    
    fbshipit-source-id: 023434b1993cc68fa286ec89087d488768669a72

--- scripts/test/test_camera.py
+++ scripts/test/test_camera.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
-# Copyright 2004-present Facebook. All Rights Reserved.
-
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
+
 
 """ Camera unit tests
 
